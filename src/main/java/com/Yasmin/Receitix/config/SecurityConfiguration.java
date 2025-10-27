07d251dcfad021eacd163a16d7d8c34a2757108b
Yasmin <yasminbraga161@gmail.com>
2025-10-27 11:14:26 -0300

    algumas implementações

--- src/main/java/com/Yasmin/Receitix/config/SecurityConfiguration.java
+++ src/main/java/com/Yasmin/Receitix/config/SecurityConfiguration.java
@@ -33,7 +33,6 @@ public class SecurityConfiguration {
 
     // Endpoints que requerem autenticação para serem acessados
     public static final String [] ENDPOINTS_WITH_AUTHENTICATION_REQUIRED = {
-            "/api/usuario/login"
     };
 
     // Endpoints que só podem ser acessador por usuários com permissão de cliente
