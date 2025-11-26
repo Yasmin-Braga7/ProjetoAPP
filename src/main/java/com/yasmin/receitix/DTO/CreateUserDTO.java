f383eebe10e5737f9defc83b9b0268735736c04f
Yasmin <yasminbraga161@gmail.com>
2025-11-26 11:05:52 -0300

    Implementações

--- src/main/java/com/yasmin/receitix/DTO/CreateUserDTO.java
+++ src/main/java/com/yasmin/receitix/DTO/CreateUserDTO.java
@@ -3,6 +3,7 @@ package com.yasmin.receitix.DTO;
 import com.yasmin.receitix.entity.RoleName;
 
 public record CreateUserDTO(
+        String nome,
         String email,
         String senha,
         RoleName role
