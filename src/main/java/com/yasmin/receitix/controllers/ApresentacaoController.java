9c6bf544d4c32445e3145f4118208867a853ce1e
Yasmin <yasminbraga161@gmail.com>
2025-12-09 19:01:09 -0300

    Alteração

---
+++ src/main/java/com/yasmin/receitix/controllers/ApresentacaoController.java
@@ -0,0 +1,16 @@
+package com.yasmin.receitix.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@CrossOrigin("*")
+
+public class ApresentacaoController {
+
+    @GetMapping("/")
+    public String home() {
+        return "forward:/index.html";
+    }
+}
